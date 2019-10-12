d927af0a0add9069ed2b6b127560ba126983a347
geoffmschaller <32310187+geoffmschaller@users.noreply.github.com>
2019-10-12 13:13:04 -0700

    Removed Default React Icons

--- client/src/components/Inflatables/Footer/Footer.js
+++ client/src/components/Inflatables/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = (props) => {
 			<img src={MAIN_LOGO} alt=""/>
 			<div className={styles.address}>135 Traffic Way - Arroyo Grande - (805) 473 - 2205</div>
 			<div className={styles.address}>200 Higuera Street - San Luis Obispo - (805) 544 - 1772</div>
-			<div className={styles.copyright}>&copy; 2019 Kaloosian INC. All rights reserved.</div>
+			<div className={styles.copyright}>&copy; 2019 Hydro Heaven Spas. All rights reserved.</div>
 		</div>
 	);
 
