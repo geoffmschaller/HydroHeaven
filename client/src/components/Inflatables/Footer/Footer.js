eceb6f0ca2ed1705d3e4e38930917c4470d5fed1
geoffmschaller <32310187+geoffmschaller@users.noreply.github.com>
2019-11-01 11:46:52 -0700

    License and copyright update

--- client/src/components/Inflatables/Footer/Footer.js
+++ client/src/components/Inflatables/Footer/Footer.js
@@ -4,10 +4,10 @@ import MAIN_LOGO from '../../../static/images/main_logo_desktop_shaddow.png';
 
 const Footer = () => (
 	<div className={styles.footer}>
-		<img src={MAIN_LOGO} alt="" />
+		<img src={MAIN_LOGO} alt=""/>
 		<div className={styles.address}>135 Traffic Way - Arroyo Grande - (805) 473 - 2205</div>
 		<div className={styles.address}>200 Higuera Street - San Luis Obispo - (805) 544 - 1772</div>
-		<div className={styles.copyright}>&copy; 2019 Hydro Heaven Spas. All rights reserved.</div>
+		<div className={styles.copyright}>&copy; {new Date().getFullYear()} Hydro Heaven Spas. All rights reserved.</div>
 	</div>
 );
 
